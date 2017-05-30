718d660cd95de59fe296a6d4539beeeebf7c8f1e
Katarzyna Wreczycka <katwre@gmail.com>
2017-05-30 15:37:15 +0200

    Improved parseDiffMeth using python sets

--- BSseq_pipeline.py
+++ BSseq_pipeline.py
@@ -93,7 +93,7 @@ OUTPUT_FILES = [
                 [ expand ( list_files_sortbam(DIR_sorted, config["SAMPLES"][sample]["files"], config["SAMPLES"][sample]["SampleID"]  )  ) for sample in config["SAMPLES"]  ],
                 
                 #               ==== rule Bam processing ======
-                [ expand ( bam_processing(METHCALLDIR, config["SAMPLES"][sampleID]["fastq_name"] )  ) for sampleID in config["SAMPLES"]  ] #had to add it to call bam_methCall for diff meth rule
+                [ expand ( bam_processing(METHCALLDIR, config["SAMPLES"][sampleID]["fastq_name"] )  ) for sampleID in config["SAMPLES"]  ], #had to add it to call bam_methCall for diff meth rule
                 
                 # ==================  FINAL REPORT =========================
                 # TODO: This needs to be editted once we determine what final reports we want to export!
@@ -101,7 +101,7 @@ OUTPUT_FILES = [
 		
 		            # diff meth
 		            # TODO: integrate it to the final report above somehow
-		            #[ DIFFMETHDIR+"".join(x)+".sorted_diffmeth.nb.html" for x in config["DIFF_METH"]  ]
+		            [ DIFFMETHDIR+"-".join(x)+".sorted_diffmeth.nb.html" for x in config["DIFF_METH"]  ]
 
 ]
 
